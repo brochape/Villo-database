73b4137cb61969d90775bdf2a73c53bc4d845fe3
t00n <antoine.carpentier.info@gmail.com>
2015-05-01 17:02:22 +0200

    [add] almost working

--- src/main.py
+++ src/main.py
@@ -14,33 +14,22 @@ dicodegros = {
     "datetime": "DATETIME"
 }
 
-def create_create_statement(name, columns, constraints):
-    statement = "CREATE TABLE IF NOT EXISTS " + name + " ("
-    for column, typ in columns.iteritems():
-        statement += column + " " + dicodegros[typ.__name__] + ","
-    if "primary" in constraints:
-        statement += "PRIMARY KEY " + str(constraints["primary"]).replace("[", "(").replace("]", ")") + ","
-    if "unique" in constraints:
-        for unik in constraints["unique"]:
-            statement += "UNIQUE " + str(unik).replace("[", "(").replace("]", ")") + ","
-    if "foreign" in constraints:
-        for key, value in constraints["foreign"].iteritems():
-            statement += "FOREIGN KEY " + key + " REFERENCES " + value
-    statement = statement[:-1] + ")"
-    print statement
-    return statement
+def listToSQL(l):
+    return str(l).replace("[", "(").replace("]", ")")
+
+def escapeToSQL(l):
+    return map(lambda s: s.replace('\xcc\x81', '').replace('\xcc\x80', '').replace(" ", "_"), l)
 
 def create_insert_statement(name, columns, row):
-    statement = "INSERT INTO " + name + " " + str(columns.keys()).replace("[", "(").replace("]", ")") + " VALUES"
-    statement += str(map(lambda s: '0' if s == 'False' else '1' if s == 'True' else s, row)).replace("[", "(").replace("]", ")")
-    print statement
+    statement = "INSERT INTO " + name + " " + listToSQL(columns) + " VALUES"
+    statement += listToSQL(map(lambda s: '0' if s == 'False' else '1' if s == 'True' else s, row))
+    # print statement
     return statement
 
-def create_table(name, columns, types, data, constraints):
-    columns = OrderedDict(zip(map(lambda s: s.replace('\xcc\x81', '').replace('\xcc\x80', '').replace(" ", "_"), columns), types))
+def create_table(name, columns, data):
     db = sqlite3.connect(DB_FILENAME)
     cursor = db.cursor()
-    cursor.execute(create_create_statement(name, columns, constraints))
+    # cursor.execute(create_create_statement(name, columns, constraints))
     for row in data:
         try:
             cursor.execute(create_insert_statement(name, columns, row))
@@ -52,12 +41,23 @@ def create_table(name, columns, types, data, constraints):
 
 
 def main():
+    # stations
     parser = CSVParser("../data/stations.csv")
-    columns,parsedData = parser.parse()
-    create_table("stations", columns, [int, str, int, int, float, float], parsedData, {"primary": ["numero"], "unique": [["nom"], ["coordonnee_X", "coordonnee_Y"]]})
+    _,parsedData = parser.parse()
+    create_table("stations", ["num", "name", "seller", "capacity", "coordX", "coordY"], parsedData)
+    # velos
     parser = CSVParser("../data/villos.csv")
-    columns, parsedData = parser.parse()
-    create_table("velos", columns, [int, datetime, str, bool], parsedData, {"primary": ["numero"]})
+    _, parsedData = parser.parse()
+    create_table("bicycles", ["id", "servicedate", "model", "state"], parsedData)
+    # utilisateurs
+    parser = XMLParser("../data/users.xml")
+    subscribers, temporary = parser.parseUsers()
+    create_table("users", ["userID", "password", "expiryDate", "card"], map(lambda sub: [sub[0], sub[4], sub[-2], sub[-1]], subscribers))
+    create_table("subs", 
+        ["userID", "RFID", "lastname", "firstname", "phone", "addresscity", "addresscp", "addressstreet", "addressnumber", "subscribeDate"],
+        map(lambda sub: [sub[i] for i in range(0,4)] + [sub[i] for i in range(5,11)], subscribers))
+    create_table("users", ["userID", "password", "expiryDate", "card"], temporary)
+    create_table("tempUsers", ["userID"], map(lambda sub: [sub[0]], temporary))
     # parser = CSVParser("../data/trips.csv")
     # columns, parsedData = parser.parse()
     # create_table("trajets", columns, [int, int, int, datetime, int, datetime], parsedData)
