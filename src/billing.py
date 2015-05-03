168a50b59a9f3615e6fb8e15c6faa12fdb84e847
t00n <antoine.carpentier.info@gmail.com>
2015-05-03 21:15:20 +0200

    [add] request to get all subscribers of a month/year

---
+++ src/billing.py
@@ -0,0 +1,59 @@
+import sqlite3
+from config import db_filename
+import datetime
+from dateutil.relativedelta import relativedelta
+
+
+SUBS_QUERY="""
+	SELECT COUNT(*) 
+	FROM users
+	INNER JOIN subs ON subs.userID = users.userID
+	WHERE users.expiryDate >= ? and users.expiryDate <= ?
+"""
+
+TEMP_USERS_QUERY="""
+	SELECT COUNT(*) 
+	FROM users
+	INNER JOIN tempUsers ON tempUsers.userID = users.userID
+	WHERE users.expiryDate >= ? and users.expiryDate <= ?
+"""
+
+def compute_dates(date):
+	try:
+		[month, year] = date.split("/")
+		dateBeg = datetime.datetime(int(year), int(month), 1)
+		dateEnd = dateBeg + relativedelta(months=+1)
+	except Exception, e:
+		dateBeg = datetime.datetime(int(date), 1, 1)
+		dateEnd = dateBeg + relativedelta(years=+1)
+	return dateBeg, dateEnd
+
+def subscribers(date):
+	""" Return number of subscribers that will pay their bill on `date` 
+		`date` is a year or a month (month/year) """
+	db = sqlite3.connect(db_filename)
+	cursor = db.cursor()
+	dateBeg, dateEnd = compute_dates(date)
+	cursor.execute(SUBS_QUERY, (dateBeg, dateEnd))
+	nb = cursor.fetchone()[0]
+	cursor.close()
+	db.close()
+	return nb
+
+def temporaryUsers(date):
+	""" Return number of temporary users on `date` 
+		`date` is "month" or "year" """
+	db = sqlite3.connect(db_filename)
+	cursor = db.cursor()
+	dateBeg, dateEnd = compute_dates(date)
+	cursor.execute(TEMP_USERS_QUERY, (dateBeg, dateEnd))
+	nb = cursor.fetchone()[0]
+	cursor.close()
+	db.close()
+	return nb
+
+if __name__ == '__main__':
+	assert(compute_dates("12/2005") == (datetime.datetime(2005, 12, 1, 0, 0), datetime.datetime(2006, 1, 1, 0, 0)))
+	assert(compute_dates("2015") == (datetime.datetime(2015, 1, 1, 0, 0), datetime.datetime(2016, 1, 1, 0, 0)))
+	assert(subscribers("2011") == 552)
+	assert(subscribers("10/2011") == 46)
